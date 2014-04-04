9550dc2d608d63293192bcdf0029ab3744cf2af2
elainekmao <elainekmao@gmail.com>
2014-04-03 20:20:55 -0400

    Added code to answer queries

--- knowledge_graph.py
+++ knowledge_graph.py
@@ -1,4 +1,4 @@
-import json, sys, string, urllib, urllib2
+import json, sys, string, urllib, urllib2, re, collections
 
 #Freebase API key = AIzaSyCnxmwlxKWsLnzs9d98rcwDhh68kuwHVXs
 #Example usage: https://www.googleapis.com/freebase/v1/search?query=bob&key=AIzaSyCnxmwlxKWsLnzs9d98rcwDhh68kuwHVXs
@@ -42,6 +42,93 @@ def query_freebase_topic(key, mid):
     else:
         del entries[:]
         return False  
+
+def answer_query(query, key):
+    valid_query = is_valid(query)
+    if valid_query:
+        url = 'https://www.googleapis.com/freebase/v1/mqlread?'
+        entity_type = get_type(valid_query,key)
+        if entity_type == '/book/book':
+            book_query = '[{ "works_written": [{ "name": null, "name~=": "' + urllib.quote(valid_query) + '" }], "name": null, "type": "/book/author" }]'
+            book_url = url + 'query=' + book_query + '&key=' + key
+            parse_answer(book_url, 'book')
+        elif entity_type == '/organization/organization':
+            organization_query = '[{ "organizations_founded": [{ "name": null, "name~=": "' + urllib.quote(valid_query) + '" }], "name": null, "type": "/organization/organization_founder" }]'
+            organization_url = url + 'query=' + organization_query + '&key=' + key
+            parse_answer(organization_url, 'organization')
+        else:
+            print valid_query + " is not a book or an organization."
+    else:
+        print "That is not a valid query."
+
+def is_valid(query):
+    matchObj = re.match("Who created (.*).* ?", query[:-1])
+    if matchObj:
+        return matchObj.group(1)
+    else:
+        return False
+
+def get_type(query,key):
+    q = {'query':query, 'key':key}
+    url = 'https://www.googleapis.com/freebase/v1/search?' + urllib.urlencode(q)
+    req = urllib2.Request(url)
+    content = json.loads(urllib2.urlopen(req).read())
+    global results
+    results = []
+    for obj in content["result"]:
+        result_type = top_freebase_topic(key, obj["mid"])
+        if result_type:   # will stop at the top-level mid containing valid entry
+            return result_type
+    return False 
+
+def top_freebase_topic(key, mid):
+    url = 'https://www.googleapis.com/freebase/v1/topic'
+    params = {
+        'key': key,
+        'filter': 'suggest'
+    }
+    url += mid + '?' + urllib.urlencode(params)
+    topic = json.loads(urllib.urlopen(url).read())
+    for result in topic["property"]["/type/object/type"]["values"]:
+        results.append(str(result["id"]))
+    validEntities = {"/book/book","/organization/organization"}
+    if any(result in validEntities for result in results):
+        for r in results:
+            if r in validEntities:
+                return r
+    else:
+        del results[:]
+        return None 
+
+def parse_answer(url, entity_type):
+    answer = json.loads(urllib.urlopen(url).read())
+    reply_dict = collections.defaultdict(list)
+    def print_sentence(entity_list, sentence, length):
+        if length == 1:
+            sentence += str(entity_list[0])
+            return sentence
+        elif length == 2:
+            sentence += str(entity_list[0]) + ', and '
+            return print_sentence(entity_list[1:], sentence, 1)
+        else:
+            sentence += str(entity_list[0]) + ', '
+            return print_sentence(entity_list[1:], sentence, length - 1)
+    if entity_type == 'organization':
+        for obj in answer["result"]:
+            name = obj["name"]
+            organizations = obj["organizations_founded"]
+            for organization in organizations:
+                reply_dict[name].append(organization["name"])
+        for person in sorted(reply_dict.keys()):
+            print person + ' (as BusinessPerson) created ' + print_sentence(reply_dict[person], '', len(reply_dict[person])) + '.'
+    elif entity_type == 'book':
+        for obj in answer["result"]:
+            name = obj["name"]
+            books = obj["works_written"]
+            for book in books:
+                reply_dict[name].append(book["name"])
+        for person in sorted(reply_dict.keys()):
+            print person + ' (as Author) created ' + print_sentence(reply_dict[person], '', len(reply_dict[person])) + '.'
 
 def usage():
     sys.stderr.write("""
